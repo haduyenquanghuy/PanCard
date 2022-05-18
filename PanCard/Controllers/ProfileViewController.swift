a89b0af6601c0af3dc9bef0f83a711a0ffc4a522
Quang Huy Ha Duyen <haduyenquanghuy@gmail.com>
2022-05-18 07:46:26 +0700

    add some ViewController, and handle basic flow

---
+++ PanCard/Controllers/ProfileViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileViewController.swift
+//  PanCard
+//
+//  Created by Ha Duyen Quang Huy on 18/05/2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.isHidden = true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
